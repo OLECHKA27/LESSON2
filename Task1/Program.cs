9caad27285e6bba703126b3ebc923cac600f99f9
RUTT <rutt@inbox.ru>
2025-02-26 23:38:50 +0300

    Изучение массивов

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,15 @@
-﻿int [] arr =new int [5];
+﻿int[] arr = new int[5];
+arr[0] = 3;
+arr[1] = 8;
+arr[2] = 45;
+arr[3] = 13;
+arr[4] = 3;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+//Console.WriteLine(arr[5]);
+int[] arr2 = new int[5] { 1, 4, 6, 9, 0 };
+int[] arr3 = {2,5,7,0,6};
