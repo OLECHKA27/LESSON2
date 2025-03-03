19b7f1ecf5e9a5f0d1ff66fc3e27a9c5837a2256
RUTT <rutt@inbox.ru>
2025-03-03 22:52:37 +0300

    Вывод массива с N и i

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N =int.Parse(Console.ReadLine());
+int[] arr = new int[N];
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"Введите {i + 1}-й элемент массива: ");
+            arr[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Введённый массив:");
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    
